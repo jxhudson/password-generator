ad041b5293e8f7efa8377895d0313483dd6d0b2e
Justin <jhud1998@gmail.com>
2023-04-11 19:21:08 -0500

    Adds Psuedo Code for Script file

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Create function generatePassword()
+// Function needs to set criteria
+// ~ Password is greater than 8 characters and less than 128 characters
+// ~ Choose between lowercase, uppercase, numeric, and/or special characters
+// ~ Validates and at least one character type selected
+// ~ Generates password that matches criteria
+
 // Write password to the #password input
 function writePassword() {
+// Variable password is a result of the function generatePassword () which doesn't exist right now
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+// Prints password to screen
   passwordText.value = password;
 
 }
