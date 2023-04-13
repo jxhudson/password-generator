79af7b73e9eece03fcbca2427a4ed40484d9b2f0
Justin <jhud1998@gmail.com>
2023-04-13 02:42:14 -0500

    Added Variables and Prompts

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var generatedpassword = [];
+var lowercaseletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialcharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '+'];
 
+function generatePassword() {
+  var passwordlength = parseInt(prompt("Choose a Password Length, It needs to be between 8-128 characters")); // Turns String into a Number
+
+  if (isFinite(passwordlength) && passwordlength < 129 && passwordlength > 7) // All must be true
+  {
+    alert('Yay'); } else { 
+      alert("Length needs to be between 8-128 characters");
+      return "Try again";
+    }
+  
+
+  if (confirm("Do you want password to contain lowercase letters?")) //
+  {
+    generatedpassword = generatedpassword.concat(lowercaseletters);
+  }
+
+  if (confirm("Do you want password to contain uppercase letters?")) //
+  {
+    generatedpassword = generatedpassword.concat(uppercaseletters);
+  }
+
+  if (confirm("Do you want password to contain numeric?")) //
+  {
+    generatedpassword = generatedpassword.concat(numeric);
+  }
+
+  if (confirm("Do you want password to contain special characters?")) //
+  {
+    generatedpassword = generatedpassword.concat(specialcharacters);
+  }
+
+}
 // Create function generatePassword()
 // Function needs to set criteria
 // ~ Password is greater than 8 characters and less than 128 characters
@@ -10,11 +47,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-// Variable password is a result of the function generatePassword () which doesn't exist right now
+  // Variable password is a result of the function generatePassword () which doesn't exist right now
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-// Prints password to screen
+  // Prints password to screen
   passwordText.value = password;
 
 }
